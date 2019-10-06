21c801d4fc3773abf9c3c4ac1551c4d5839a1c2e
Clarissa Borges <clarissalimab08@gmail.com>
2019-10-06 13:17:14 -0300

    Add script to seed db

--- populate.py
+++ populate.py
@@ -2,19 +2,17 @@ from project.api.planting_status.models import Machines, Plantings, Seedlings
 
 
 def seedMachine(db):
-    db.session.add(Machines(pincode=1234, raspberry_ip='192.168.1.8', currently_backlit=False, currently_irrigating=False, smart_irrigation_enabled=False, smart_illumination_enabled=False, planting_active=False))
+    db.session.add(Machines(pincode=1234, raspberry_ip='127.0.0.1', currently_backlit=False, currently_irrigating=False, smart_irrigation_enabled=False, smart_illumination_enabled=False, planting_active=False))
     db.session.commit()
 
 
 def seedPlanting(db):
     machine = Machines.query.first()
     seedling = Seedlings.query.first()
-    db.session.add(Plantings(name='alface', planting_date='2019-09-29 09:26:03.478039', sprouted_seedlings=0, current_humidity=20, current_temperature=30, hours_backlit=5, cycle_finished=False, picture_url='url', machine=machine, seedling=seedling))
-    
-    db.session.add(Plantings(name='Tomate', planting_date='2018-05-19 15:26:03.478039', sprouted_seedlings=0, current_humidity=20, current_temperature=30, hours_backlit=5, cycle_finished=True, picture_url='url', machine=machine, seedling=seedling))
+    db.session.add(Plantings(name='fejao', planting_date='2019-10-06 15:43:40.300649', sprouted_seedlings=0, current_humidity=20, current_temperature=30, hours_backlit=5, cycle_finished=True, picture_url='url', machine=machine, seedling=seedling))
     db.session.commit()
 
 
 def seedSeedlings(db):
-    db.session.add(Seedlings(name='alface', average_harvest_time=25, humidity_threshold=40))
+    db.session.add(Seedlings(name='fejao', average_harvest_time=20, humidity_threshold=50))
     db.session.commit()
