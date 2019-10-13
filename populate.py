e0a10478f15b4e576c1ec1fc15a3863d8e3eb5fd
Mariana Picolo <marianapicolo@hotmail.com>
2019-10-12 21:41:52 -0300

    Add remaining days of current cycle

--- populate.py
+++ populate.py
@@ -9,12 +9,12 @@ def seedMachine(db):
 def seedPlanting(db):
     machine = Machines.query.first()
     seedling = Seedlings.query.first()
-    db.session.add(Plantings(name='alface', planting_date='2019-09-29 09:26:03.478039', sprouted_seedlings=0, current_humidity=20, current_temperature=30, hours_backlit=5, cycle_finished=False, picture_url='url', machine=machine, seedling=seedling))
+    db.session.add(Plantings(name='Alface', planting_date='2019-09-29 09:26:03.478039', sprouted_seedlings=0, current_humidity=20, current_temperature=30, hours_backlit=5, cycle_finished=False, picture_url='url', machine=machine, seedling=seedling))
     
-    db.session.add(Plantings(name='Tomate', planting_date='2018-05-19 15:26:03.478039', sprouted_seedlings=0, current_humidity=20, current_temperature=30, hours_backlit=5, cycle_finished=True, picture_url='url', machine=machine, seedling=seedling))
+    db.session.add(Plantings(name='Tomate', planting_date='2018-08-19 15:26:03.478039', sprouted_seedlings=0, current_humidity=30, current_temperature=25, hours_backlit=15, cycle_finished=True, picture_url='url', machine=machine, seedling=seedling))
     db.session.commit()
 
 
 def seedSeedlings(db):
-    db.session.add(Seedlings(name='alface', average_harvest_time=25, humidity_threshold=40))
+    db.session.add(Seedlings(name='Alface', average_harvest_time=25, humidity_threshold=40))
     db.session.commit()
